730cbda7e6644c11ed36e796d40a0c239b2c5ea9
totoro <totoro.fu@webeye.com>
2024-10-15 11:52:08 +0800

    代码优化

---
+++ 1.threejs快速入门/12.WebGL渲染器设置(背景透明度)/model.js
@@ -0,0 +1,9 @@
+import * as THREE from "three";
+
+const geometry = new THREE.BoxGeometry(120, 120, 120);
+const material = new THREE.MeshLambertMaterial({
+  color: 0x00ffff,
+});
+const mesh = new THREE.Mesh(geometry, material);
+
+export default mesh;
