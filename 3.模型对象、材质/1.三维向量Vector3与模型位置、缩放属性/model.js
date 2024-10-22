7c84746ebc9fa00fe97d578b1b3c9bf3eb161045
totoro <totoro.fu@webeye.com>
2024-10-22 19:02:26 +0800

    代码优化

--- 3.模型对象、材质/1.三维向量Vector3与模型位置、缩放属性/model.js
+++ 3.模型对象、材质/1.三维向量Vector3与模型位置、缩放属性/model.js
@@ -6,8 +6,9 @@ const material = new THREE.MeshLambertMaterial({
   transparent: true,
   opacity: 0.5,
 });
-geometry.translate(0, 0, 50); // 并不会改变mesh.position
-geometry.rotateY(Math.PI / 3);
+geometry.translate(0, 0, 25); // 并不会改变mesh.position
+geometry.rotateY(Math.PI / 2); // 逆时针
+
 const mesh = new THREE.Mesh(geometry, material);
 
 // 位置属性.position使用threejs三维向量对象Vector3表示的
@@ -16,7 +17,8 @@ console.log("模型位置属性.position的值", mesh.position); // 0, 0, 0
 // .position的值是Vector3，意味着你想改变.position,可以查询文档Vector3类
 // 直接设置网格模型的位置
 mesh.position.set(0, 0, 0);
-// mesh.position.x = 100; // 设置模型的x坐标
+mesh.position.x = 50; // 设置模型的x坐标
+mesh.add(new THREE.AxesHelper(50)); // mesh的局部坐标系
 
 //沿着axis轴表示方向平移100
 // mesh.translateOnAxis(new THREE.Vector3(1, 1, 1).normalize(), 100);
--- 3.模型对象、材质/2.欧拉Euler与角度属性.rotation/model.js
+++ 3.模型对象、材质/2.欧拉Euler与角度属性.rotation/model.js
@@ -24,12 +24,12 @@ mesh.rotation.y += Math.PI / 3;
 //绕y轴的角度减去60度
 mesh.rotation.y -= Math.PI / 3;
 
-// 通过模型的旋转方法，也可以改变角度属性.rotation
-mesh.rotateY(Math.PI / 2); // 绕着Y轴旋转90度
-//控制台查看：旋转方法，改变了rotation属性
-console.log("mesh.rotation", mesh.rotation);
+// // 通过模型的旋转方法，也可以改变角度属性.rotation
+// mesh.rotateY(Math.PI / 2); // 绕着Y轴旋转90度
+// //控制台查看：旋转方法，改变了rotation属性
+// console.log("mesh.rotation", mesh.rotation);
 
-const axis = new THREE.Vector3(0, 0, 1); //向量axis
-mesh.rotateOnAxis(axis, Math.PI / 8); //绕axis轴旋转π/8
+// const axis = new THREE.Vector3(0, 0, 1); //向量axis
+// mesh.rotateOnAxis(axis, Math.PI / 8); //绕axis轴旋转π/8
 
 export default mesh;
